abc796f4a4a04c76b7bca2b2ead3eb93d889fd0a
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2025-07-13 17:02:18 +0530

    Updated image enhancement, removed brightness and contrast

--- Image-Processor/enhance.c
+++ Image-Processor/enhance.c
@@ -1,13 +1,21 @@
 #include "enhance.h"
 
 void enhance_image(unsigned char img[32][32]) {
+    int min = 255, max = 0;
+    for (int i = 0; i < 32; i++)
+        for (int j = 0; j < 32; j++) {
+            if (img[i][j] < min) min = img[i][j];
+            if (img[i][j] > max) max = img[i][j];
+        }
+
     for (int i = 0; i < 32; i++) {
         for (int j = 0; j < 32; j++) {
             int val = img[i][j];
-            val = val * 1.2 + 15; // brightness + contrast
-            if (val > 255) val = 255;
-            if (val < 0) val = 0;
-            img[i][j] = val;
+            if (max > min)
+                val = (val - min) * 255 / (max - min);
+            else
+                val = 0;
+            img[i][j] = (unsigned char)val;
         }
     }
 }
