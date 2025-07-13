4c5b94271550ae31c590f33ccd95df332c75dc7c
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2025-07-13 17:04:06 +0530

    Added efficient resizing logic

--- Image-Processor/resize.c
+++ Image-Processor/resize.c
@@ -2,14 +2,27 @@
 #include <string.h>
 
 void resize_to_32x32(unsigned char **input, int w, int h, unsigned char output[32][32]) {
+    memset(output, 0, 32 * 32);
+
+    int x_offset = 0, y_offset = 0;
+    int crop_w = w, crop_h = h;
+
+    if (w > 32) {
+        x_offset = (w - 32) / 2;
+        crop_w = 32;
+    }
+    if (h > 32) {
+        y_offset = (h - 32) / 2;
+        crop_h = 32;
+    }
+
     for (int i = 0; i < 32; i++) {
+        int src_y = y_offset + i * crop_h / 32;
+        if (src_y >= h) continue;
         for (int j = 0; j < 32; j++) {
-            int y = i * h / 32;
-            int x = j * w / 32;
-            if (y < h && x < w)
-                output[i][j] = input[y][x];
-            else
-                output[i][j] = 0;
+            int src_x = x_offset + j * crop_w / 32;
+            if (src_x >= w) continue;
+            output[i][j] = input[src_y][src_x];
         }
     }
 }
