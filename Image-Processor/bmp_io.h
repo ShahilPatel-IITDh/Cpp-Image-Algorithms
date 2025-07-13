059c9a8ddff0fec5c3a3473d0021344503bb3a54
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2025-07-13 17:01:39 +0530

    Adding alloc_image function header

--- Image-Processor/bmp_io.h
+++ Image-Processor/bmp_io.h
@@ -3,6 +3,7 @@
 
 unsigned char **load_bmp(const char *filename, int *width, int *height);
 void save_bmp(const char *filename, unsigned char image[32][32], int width, int height);
+unsigned char **alloc_image(int width, int height);
 void free_image(unsigned char **image, int height);
 
 #endif
